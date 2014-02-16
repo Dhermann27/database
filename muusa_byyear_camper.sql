cad7fc848021aee5f6bea3dc4d51906243244db7
Dan Hermann <dh78@me.com>
2014-02-16 12:07:26 -0600

    Added Paydate

--- muusa_byyear_camper.sql
+++ muusa_byyear_camper.sql
@@ -8,7 +8,7 @@ CREATE VIEW muusa_byyear_camper AS
     	c.gradeoffset, muusa_getage(c.birthdate, ya.year)+c.gradeoffset grade, 
     	p.id programid, p.name programname, c.sponsor, c.is_handicap, c.foodoptionid, 
     	u.id churchiid, u.name churchname, u.city churchcity, u.statecd churchstatecd, 
-    	ya.id yearattendingid, ya.days, ya.roomid, r.roomnbr, b.id buildingid, b.name buildingname
+    	ya.id yearattendingid, ya.paydate, ya.days, ya.roomid, r.roomnbr, b.id buildingid, b.name buildingname
     FROM (muusa_family f, muusa_camper c, muusa_yearattending ya, muusa_program p) 
     LEFT JOIN (muusa_building b, muusa_room r) ON ya.roomid=r.id AND r.buildingid=b.id 
     LEFT JOIN muusa_church u ON c.churchid=u.id
--- muusa_thisyear_camper.sql
+++ muusa_thisyear_camper.sql
@@ -3,7 +3,7 @@ CREATE VIEW muusa_thisyear_camper AS
 	SELECT familyid, familyname, address1, address2, city, statecd, zipcd, country, 
     	id, sexcd, firstname, lastname, email, birthdate, birthday, age, 
     	gradeoffset, grade, programid, programname, sponsor, is_handicap, foodoptionid, 
-    	churchiid, churchname, churchcity, churchstatecd, yearattendingid, days, 
+    	churchiid, churchname, churchcity, churchstatecd, yearattendingid, paydate, days, 
     	roomid, roomnbr, buildingid, buildingname
     FROM muusa_byyear_camper bc, muusa_year y 
     WHERE bc.year=y.year AND y.is_current=1&
